6c1aeb5d0ac6da584e5138ec12f946612fc6dea5
razvanmocanu <razvan.mocanu93@gmail.com>
2014-05-21 16:52:39 +0300

    Role

--- com.bergcomputers.bcibpersistencemodel/src/main/java/com/bergcomputers/domain/Role.java
+++ com.bergcomputers.bcibpersistencemodel/src/main/java/com/bergcomputers/domain/Role.java
@@ -1,5 +1,25 @@
 package com.bergcomputers.domain;
 
-public class Role extends BaseEntity {
+import javax.persistence.Entity;
 
+@Entity
+public class Role extends BaseEntity implements IRole {
+	private String name;
+
+	/* (non-Javadoc)
+	 * @see com.bergcomputers.domain.IRole#getName()
+	 */
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.bergcomputers.domain.IRole#setName(java.lang.String)
+	 */
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
