8e12ca71a5ee8b03b1058516f6386b76c3555b2c
dalexandra <duduiala.alexandra@yahoo.com>
2014-07-09 18:11:32 +0300

    create & return at getTransaction

--- com.bergcomputers.bcibrestws/src/main/java/com/bergcomputers/rest/accounts/TransactionsResource.java
+++ com.bergcomputers.bcibrestws/src/main/java/com/bergcomputers/rest/accounts/TransactionsResource.java
@@ -1,12 +1,15 @@
 package com.bergcomputers.rest.accounts;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,6 +18,7 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 
 import com.bergcomputers.domain.Account;
 import com.bergcomputers.domain.Transaction;
@@ -47,17 +51,38 @@ public class TransactionsResource {
     @Path("/detail/{transactionid}")
     @Produces("application/json")
     public Transaction getTransactionDetails(@PathParam("transactionid") Long transactionid) {
-     return transactionController.get(transactionid);
+     return transactionController.findTransaction(transactionid);
 	}
+    @GET
+    @Produces("application/json")
+    @Path("uris")
+    public String getTransactionsURIs() {
+        JSONArray uriArray = new JSONArray();
+             for (Transaction transaction :  transactionController.getTransactions()) {
+                UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+                URI transactionUri = ub.path(((Transaction)transaction).getId().toString()).build();
+                uriArray.put(transactionUri.toASCIIString());
+            }
+        return uriArray.toString();
+    }
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Transaction createTransaction(final Transaction jsonTransaction) throws JSONException {
+
+    	jsonTransaction.setCreationDate(null ==jsonTransaction.getCreationDate() ? new Date():jsonTransaction.getCreationDate());
+    	Transaction transactionEntity = transactionController.create(jsonTransaction);
+        return transactionEntity;
+    }
 
 	/*
 	 * @PUT
 	 * 
 	 * @Consumes("application/json")
-	 * 
-	 * @Produces("application/json") public Transaction createTransaction(final
-	 * Transaction jsonTransaction) throws JSOnException{
-	 * jsonTransaction.setCreationDate(null=jsonTransaction.getCreationDate()) }
+	 * @Produces("application/json")
+	 *  public Transaction createTransaction(final Transaction jsonTransaction) throws JSOnException{
+	 * jsonTransaction.setCreationDate(null=jsonTransaction.getCreationDate()) 
+	 * }
 	 */
 	@DELETE
 	@Path("/{transactionid}")
